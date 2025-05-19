53bbce2747838a9e5e4a396fbd1719a6eac0c1d4
mathurin Kimbembe <mathurin.kimbembe@k-tns.com>
2025-05-19 11:23:28 +0200

    fix: i18n

---
+++ src/boot/i18n.js
@@ -0,0 +1,26 @@
+// import { boot } from 'quasar/wrappers'
+// import { createI18n } from 'vue-i18n'
+// import messages from 'src/i18n'
+// // import Helpers from 'boot/helpers'
+// import dayjs from 'dayjs'
+// import 'dayjs/locale/fr'
+//
+// dayjs.locale('fr')
+//
+// const i18n = createI18n({
+//   legacy: false,
+//   locale: localStorage.getItem('lang') ?? 'fr',
+//   // locale: 'fr-fr',
+//   fallbackLocale: 'fr',
+//   globalInjection: true,
+//   silentTranslationWarn: true,
+//   warnHtmlMessage: false,
+//   messages
+// })
+//
+// export default boot(({ app }) => {
+//   // Set i18n instance on app
+//   app.use(i18n)
+// })
+//
+// export { i18n }
