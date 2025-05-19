d373266382643df40f0625947cfc7df14021b3d8
mathurin Kimbembe <mathurin.kimbembe@k-tns.com>
2025-05-19 11:20:00 +0200

    feat: manage project

---
+++ src/modules/Projects/store/projectStore.js
@@ -0,0 +1,80 @@
+import {acceptHMRUpdate, defineStore} from 'pinia'
+import {axiosInstance} from 'boot/axios.js'
+
+import {ref} from 'vue'
+
+export const useProjectStore = defineStore('project', () => {
+  const projects = ref([])
+  const currentProject = ref(null)
+
+  async function initProject() {
+    try {
+      const response = await axiosInstance.get('/project/allProject')
+      return response.data.data
+    } catch (error) {
+      console.error('Erreur est', error)
+    }
+  }
+
+  async function setProjects() {
+    try {
+      projects.value = await initProject()
+    } catch (error) {
+      console.error('Erreur est', error)
+    }
+  }
+
+  function setCurrentProject (project) {
+    console.log('le projet reçu est', project)
+    currentProject.value = project
+    console.log('le current project est',  currentProject.value)
+  }
+
+  function getAllProjects() {
+    return projects.value
+  }
+
+  function getCurrentProject () {
+    return currentProject.value ?? null
+  }
+
+  function createProject (payload) {
+    return new Promise((resolve, reject) => {
+      axiosInstance.post('/project/create', payload)
+        .then((response) => resolve(response?.data))
+        .catch(error => reject(error))
+    })
+  }
+
+  function updateProject (payload) {
+    return new Promise((resolve, reject) => {
+      axiosInstance.put('/project/'+ payload.id, payload)
+        .then((response) => resolve(response?.data))
+        .catch(error => reject(error))
+    })
+  }
+
+  function deleteProject (projectId) {
+    return new Promise((resolve, reject) => {
+      axiosInstance.delete('/project/delete/'+ projectId)
+        .then((response) => resolve(response?.data))
+        .catch(error => reject(error))
+    })
+  }
+
+  return {
+    currentProject,
+    getCurrentProject,
+    setCurrentProject,
+    projects,
+    deleteProject,
+    createProject,
+    updateProject,
+    setProjects,
+    getAllProjects
+  }
+})
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useProjectStore, import.meta.hot))
+}
